17ded371d1c25b4417cb77189dab0863d55d8af4
Saida Davis <saihdavis@gmail.com>
2020-07-19 02:57:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,12 @@
-def prime?(integer)
-  if integer % 2 
-    return true 
-  elsif !integer % 2
-    return false 
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_to_test|
+      number % num_to_test == 0
+    end
+  else
+    false
+  end
 end
-end
+© 2020 GitHub, Inc.
